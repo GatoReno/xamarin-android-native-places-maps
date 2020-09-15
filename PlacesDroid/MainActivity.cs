4b542d7a9095ad21fbbf46aba94cf3b9a8ac90ec
Gatoreno <erp.reno@hotmail.com>
2020-09-15 19:23:06 -0400

    missing permision and geoloc

---
+++ PlacesDroid/MainActivity.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.Content;
+using Android.Gms.Maps;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Google.Places;
+
+namespace PlacesDroid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, IOnMapReadyCallback
+    {
+        GoogleMap mainMap;
+        TextView txt, txt2, txt3, txt4;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+
+
+            txt = (TextView)FindViewById(Resource.Id.txt1);
+            txt2 = (TextView)FindViewById(Resource.Id.txt2);
+            txt3 = (TextView)FindViewById(Resource.Id.txt3);
+            txt4 = (TextView)FindViewById(Resource.Id.txt4);
+
+            if (!PlacesApi.IsInitialized)
+            {
+                PlacesApi.Initialize(this, "AIzaSyD2J-IFSewL3lC7F8YucJB6JAkbb_IlSkQ");
+            }
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+
+            List<Place.Field> fields = new List<Place.Field>();
+            fields.Add(Place.Field.Id);
+            fields.Add(Place.Field.Name);
+            fields.Add(Place.Field.LatLng);
+            fields.Add(Place.Field.Address);
+
+            Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.Overlay, fields)
+                .SetCountry("Us")
+                .Build(this);
+            StartActivityForResult(intent, 0);
+            /* View view = (View)sender;
+             Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                 .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();*/
+        }
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            var place = Autocomplete.GetPlaceFromIntent(data);
+            txt.Text = place.Address;
+            txt2.Text = place.Name;
+            //txt3.Text = place.Rating.ToString();
+            txt4.Text = place.LatLng.Longitude.ToString()+ " "+ place.LatLng.Latitude.ToString();
+        }
+
+
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            //bool success = googleMap.SetMapStyle(MapStyleOptions.LoadRawResourceStyle(this, Resource.Raw.mapStyle));
+            mainMap = googleMap;
+        }
+    }
+}
